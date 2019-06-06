d7963e44a85d5e2b363935372eefa0c68421e7e0
Solarex <rh.hou.work@gmail.com>
2019-06-06 14:57:30 +0800

    output result

--- src/main/dart/ch08/ch0805.dart
+++ src/main/dart/ch08/ch0805.dart
@@ -24,3 +24,23 @@ void main() {
   scheduleMicrotask(() => print('microtask #3 of 3'));
   print('main #2 of 2');
 }
+
+/*
+main #1 of 2
+main #2 of 2
+microtask #1 of 3
+microtask #2 of 3
+microtask #3 of 3
+future #2 of 4
+future #2a
+future #2b
+future #2c
+microtask #0 (from future #2b)
+future #3 of 4
+future #4 of 4
+future #4b
+future #3a (a new future)
+future #3b
+future #4a
+future #1 (delayed)
+ */
